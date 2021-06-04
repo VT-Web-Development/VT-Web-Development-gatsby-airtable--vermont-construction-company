a8f3c1e2e77dca1cbf290b21c363ac47a4ee7040
VT-Web-Development <hellokrittech@gmail.com>
2021-06-03 22:28:16 -0400

    Basic setup

---
+++ src/components/dist/index.dev.js
@@ -0,0 +1,63 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Layout", {
+  enumerable: true,
+  get: function get() {
+    return _Layout["default"];
+  }
+});
+Object.defineProperty(exports, "Hero", {
+  enumerable: true,
+  get: function get() {
+    return _Hero["default"];
+  }
+});
+Object.defineProperty(exports, "About", {
+  enumerable: true,
+  get: function get() {
+    return _About["default"];
+  }
+});
+Object.defineProperty(exports, "Projects", {
+  enumerable: true,
+  get: function get() {
+    return _Projects["default"];
+  }
+});
+Object.defineProperty(exports, "Survey", {
+  enumerable: true,
+  get: function get() {
+    return _Survey["default"];
+  }
+});
+Object.defineProperty(exports, "Slider", {
+  enumerable: true,
+  get: function get() {
+    return _Slider["default"];
+  }
+});
+Object.defineProperty(exports, "GridProjects", {
+  enumerable: true,
+  get: function get() {
+    return _GridProjects["default"];
+  }
+});
+
+var _Layout = _interopRequireDefault(require("./Layout"));
+
+var _Hero = _interopRequireDefault(require("./Hero"));
+
+var _About = _interopRequireDefault(require("./About"));
+
+var _Projects = _interopRequireDefault(require("./Projects"));
+
+var _Survey = _interopRequireDefault(require("./Survey"));
+
+var _Slider = _interopRequireDefault(require("./Slider"));
+
+var _GridProjects = _interopRequireDefault(require("./GridProjects"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
